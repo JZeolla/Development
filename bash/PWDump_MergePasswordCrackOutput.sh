fa9fdc47f1cf528611314b7db0e44105053ac56d
JZeolla <Zeolla@gmail.com>
2015-02-09 14:51:58 -0500

    Update PWDump_MergePasswordCrackOutput.sh

--- bash/PWDump_MergePasswordCrackOutput.sh
+++ bash/PWDump_MergePasswordCrackOutput.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#To enable and disable tracing use:  set -x (On) set +x (Off)
+# To enable and disable tracing use:  set -x (On) set +x (Off)
 
 # =========================
 # Author:         Jon Zeolla (JZeolla)
