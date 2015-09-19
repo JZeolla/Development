6986c518867b9edac811d2dc1b0672357818db96
JonZeolla <Zeolla@gmail.com>
2015-09-19 17:19:10 -0400

    Update tune-network-interface.sh

--- Bash/tune-network-interface.sh
+++ Bash/tune-network-interface.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 # To enable and disable tracing use:  set -x (On) set +x (Off)
 
+### BEGIN INIT INFO
+# Provides:          tune-network-interface
+# Required-Start:    $remote_fs $syslog $network
+# Required-Stop:     
+# Default-Start:     2 3 4 5
+# Default-Stop:      
+# Short-Description: Tune the NIC(s) to be properly configured sniffers
+### END INIT INFO
+
 # =========================
 # Author:          Jon Zeolla (JZeolla)
 # Last update:     2015-07-28
 # File Type:       Bash Script
 # Version:         1.1
 # Repository:      https://github.com/JonZeolla
-# Description:     This is a basic script meant to be run via a cron at @reboot to ensure persistent NIC settings for systems that evaluate network traffic (such as IDSs).
+# Description:     This is a basic script meant to be run during startup to ensure persistent NIC settings for systems that evaluate network traffic (such as IDSs).  It was designed to use @reboot via a cron, or rc.local to run during startup.  
 #
 # Notes
 # - Reference the blog post at http://pevma.blogspot.com/2014/03/suricata-prepearing-10gbps-network.html
@@ -15,32 +24,62 @@
 #
 # =========================
 
-# Set the interfaces which are being used as sniffers and iterate through them
+## Begin Logging stdout and stderr
+exec 1> >(logger -s -t $(basename ${0}) -p local1.info)
+exec 2> >(logger -s -t $(basename ${0}) -p local1.err)
+
+## Global Instantiations
+# Variables
+EXIT_CODE=0 # Script exit code
+
+
+## Functions
+function errorecho() {
+	>&2 echo "${1}"
+}
+
+function error_out() {
+	ERROR=${1}
+	# Known error codes to ignore because they occur when you attempt to configure a setting via ethtool that is already set
+	if [[ ${ERROR} -ne 80 && ${ERROR} -ne 89 ]]; then
+		EXIT_CODE=${1}
+	        errorecho "ERROR on $(hostname) while running $(readlink -f ${0}) at $(date +%Y-%m-%d_%H:%M) with code ${EXIT_CODE}"
+		exit ${1}
+	fi
+}
+
+# trap and call error_out()
+trap 'error_out 1' SIGINT SIGTERM SIGHUP SIGKILL
+
+## Set the interfaces which are being used as sniffers and iterate through them
 for interface in eth0
 do
         # Disable NIC offloading for everything to ensure that the Suricata process sees everything as it was on the wire
-        ethtool -K "${interface}" rx off
-        ethtool -K "${interface}" tx off
-        ethtool -K "${interface}" sg off
-        ethtool -K "${interface}" tso off
-        ethtool -K "${interface}" ufo off
-        ethtool -K "${interface}" gso off
-        ethtool -K "${interface}" gro off
-        ethtool -K "${interface}" lro off
-        ethtool -K "${interface}" rxvlan off
-        ethtool -K "${interface}" txvlan off
-        ethtool -K "${interface}" rxhash off
+        ethtool -K "${interface}" rx off || error_out "$?"
+        ethtool -K "${interface}" tx off || error_out "$?"
+        ethtool -K "${interface}" sg off || error_out "$?"
+        ethtool -K "${interface}" tso off || error_out "$?"
+        ethtool -K "${interface}" ufo off || error_out "$?"
+        ethtool -K "${interface}" gso off || error_out "$?"
+        ethtool -K "${interface}" gro off || error_out "$?"
+        ethtool -K "${interface}" lro off || error_out "$?"
+        ethtool -K "${interface}" rxvlan off || error_out "$?"
+        ethtool -K "${interface}" txvlan off || error_out "$?"
+        ethtool -K "${interface}" rxhash off || error_out "$?"
 
         # Set the rx ring parameter to the pre-set max
-        ethtool -G "${interface}" rx 4096
+        ethtool -G "${interface}" rx 4096 || error_out "$?"
 
         # Load balance UDP flows using {IP Src, IP Dst, Src Port, Dst Port} for UDPv4 and UDPv6 instead of the default {IP Src, IP Dst}
         ethtool -N "${interface}" rx-flow-hash udp4 sdfn
         ethtool -N "${interface}" rx-flow-hash udp6 sdfn
 
         # Generate an interrupt after each frame/millisecond, minimizing latency but increasing load
-        ethtool -C "${interface}" rx-usecs 1 rx-frames 0
+        ethtool -C "${interface}" rx-usecs 1 rx-frames 0 || error_out "$?"
 
         # Disable adaptive interrupt for rx because we are ok with up to one interrupt per received frame
-        ethtool -C "${interface}" adaptive-rx off
+        ethtool -C "${interface}" adaptive-rx off || error_out "$?"
 done
+
+## Stop Logging and exit appropriately
+echo "$(hostname):$(readlink -f ${0}) completed at [`date`] as PID $$ with $- flags and an exit code of ${EXIT_CODE}"
