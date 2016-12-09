f7f6de7bd804921f10b217a56033e31a40281170
JonZeolla <Zeolla@gmail.com>
2016-12-09 13:12:15 -0500

    Update generate_csr.sh
    Variables derived from user input should always be quoted.  Also, make this slightly more portable to OSX.

--- Bash/generate_csr.sh
+++ Bash/generate_csr.sh
@@ -4,9 +4,9 @@
 
 # =========================
 # Author:          Jon Zeolla (JZeolla, JonZeolla)
-# Last update:     2016-06-21
+# Last update:     2016-12-09
 # File Type:       Bash Script
-# Version:         1.1
+# Version:         1.2
 # Repository:      https://github.com/JonZeolla/Development
 # Description:     This is a simple bash script to create a certificate signing request.
 #
@@ -47,22 +47,22 @@ function checkInput() {
     ## Clean up the input
     sanitizeInput="${1//[!a-zA-Z0-9\.\-\*]/}"
 
-    if [[ ${sanitizeInput} == \** ]]; then
+    if [[ "${sanitizeInput}" == \** ]]; then
         # There is at least one asterisk and it is at the beginning.
         # If there is more than one, remove the second one to break checks later on
-        cleanInput=$(sed 's/\*//2' <<< ${sanitizeInput})
-    elif [[ ${sanitizeInput} != *\** && ${sanitizeInput} != "" ]]; then
+        cleanInput=$(sed 's/\*//2' <<< "${sanitizeInput}")
+    elif [[ "${sanitizeInput}" != *\** && "${sanitizeInput}" != "" ]]; then
         # There is no asterisk
-        cleanInput=${sanitizeInput}
+        cleanInput="${sanitizeInput}"
 
         # Check to see if it's an IP - IPv6 is not yet supported due to its detection complexity
-        if [[ ${cleanInput} =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-            origIFS=${IFS}
+        if [[ "${cleanInput}" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+            origIFS="${IFS}"
             IFS='.'
             ip=(${cleanInput})
-            IFS=${origIFS}
-            if [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]; then
-                isIP=1
+            IFS="${origIFS}"
+            if [[ "${ip[0]}" -le 255 && "${ip[1]}" -le 255 && "{ip[2]}" -le 255 && "${ip[3]}" -le 255 ]]; then
+                isIP="1"
             fi
         fi
     else
@@ -85,7 +85,7 @@ isIP=0
 echo -e "\nDon't forget to run this in the directory that you have your key\n"
 echo "What should the CN be for the certificate?  Your current hostname is ${HOSTNAME}"
 read -r CN
-checkInput ${CN}
+checkInput "${CN}"
 
 until [[ "${CN}" == "${cleanInput}" && "${isIP}" == 0 ]]; do
     if [[ "${isIP}" != 0 ]]; then
@@ -105,9 +105,9 @@ fi
 echo "Generating random data..."
 dd if=/dev/urandom of=/tmp/random.data bs=4096k count=1 2> /dev/null
 
-if [[ ! -e ${CN}.key ]]; then
+if [[ ! -e "${CN}.key" ]]; then
     echo "Generating cert..."
-    openssl genrsa -rand /tmp/random.data -out ${CN}.key 4096
+    openssl genrsa -rand /tmp/random.data -out "${CN}.key" 4096
 fi
 
 echo "Configuring OpenSSL..."
@@ -142,8 +142,8 @@ subjectAltName = @alt_names
     echo -e "\nPlease list your SANs and end them with a blank line"
     i=1
     while read -r line; do
-        [[ -z ${line} ]] && break
-	checkInput ${line}
+        [[ -z "${line}" ]] && break
+	checkInput "${line}"
         if [[ "${isIP}" != 0 ]]; then
             echo -e "\nThe previous SAN will not be used as it contains an IPv4 address.  The SAN you provided was ${line}.  If necessary, please continue entering SANs.\n"
         elif [[ "${line}" != "${cleanInput}" ]]; then
@@ -160,16 +160,20 @@ fi
 echo -e "Creating CSR...\n\n"
 echo -e "Please email the following to your certificate authority\n"
 echo -n "Name: "
-getent passwd ${SUDO_USER:-$USER} | cut -d ':' -f 5 | cut -d ',' -f 1
+if command -v getent > /dev/null 2>&1 ; then
+    getent passwd "${SUDO_USER:-$USER}" | cut -d ':' -f 5 | cut -d ',' -f 1
+else
+    id -F
+fi
 echo "Purpose: ${purpose}
 X.509 Values:"
 grep "\[Subject_Name\]" -A 6 /tmp/openssl.cnf
-if [[ -n ${SAN} ]]; then
+if [[ -n "${SAN}" ]]; then
     echo -e "\nThe following SANs should already be included in the certificate request:"
     echo "${SAN[@]}" | sed 's_\s\+_\n_g'
 fi
 echo
 
-openssl req -new -key ${CN}.key -config /tmp/openssl.cnf -batch || cleanup "error"
-chmod o-rwx ${CN}.key
+openssl req -new -key "${CN}.key" -config /tmp/openssl.cnf -batch || cleanup "error"
+chmod o-rwx "${CN}.key"
 cleanup "quit"
