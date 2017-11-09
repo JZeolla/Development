07a2ac2f3fbc65ad372264a74cd75cf0d302feec
JonZeolla <Zeolla@gmail.com>
2017-11-09 14:20:02 -0500

    Add comments to List-AllAzureRmVMs.ps1

--- powershell/List-AllAzureRmVMs.ps1
+++ powershell/List-AllAzureRmVMs.ps1
@@ -1,4 +1,22 @@
-  Import-Module AzureRm
+# =========================
+# Author:          Jon Zeolla (JZeolla)
+# Last update:     2017-11-09
+# File Type:       PowerShell Script
+# Version:         1.0
+# Repository:      https://github.com/JonZeolla/Development
+# Description:     This is a PowerShell script to pull the public and private IPs for AzureRm VMs in a given Azure tenant.
+#
+# Notes
+# - Anything that has a placeholder value is tagged with TODO.
+# - This can be useful to audit an environment, or to do immediately prior to doing a vulnerability scan of AzureRM VMs (to get an updated IP list).
+# - If attempting to do something similar with the Azure Cloud Shell (bash), consider:
+#    ```
+#    az account list | jq -r '.[] | .id' | while read subs; do echo "${subs}"; az account set --subscription "${subs}"; az vm list-ip-addresses | jq '.[] | .virtualMachine.network.publicIpAddresses | .[] | .ipAddress'; done
+#    ```
+#
+# =========================
+
+Import-Module AzureRm
 
 $VMList = @()
 
