5d8113c294221bf0b058996e05b82b66506e883a
JonZeolla <Zeolla@gmail.com>
2018-03-05 14:10:51 -0500

    Escaping is only necessary if this is a one-liner

--- python/librdkafka_version_checker.py
+++ python/librdkafka_version_checker.py
@@ -1,5 +1,5 @@
 from ctypes import *
 minver = 0x904ff
-dll = cdll.LoadLibrary(\"/usr/local/lib/librdkafka.so\")
+dll = cdll.LoadLibrary("/usr/local/lib/librdkafka.so")
 version = dll.rd_kafka_version()
 exit(0) if minver >= version else exit(1)
