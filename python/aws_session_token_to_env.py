9d36503b260d99f79110235841e4c0565f2cdfab
JonZeolla <Zeolla@gmail.com>
2019-01-07 10:07:49 -0500

    Update aws_session_token_to_env.py

--- python/aws_session_token_to_env.py
+++ python/aws_session_token_to_env.py
@@ -6,10 +6,10 @@
 # File Type:       Python Script
 # Version:         0.1
 # Repository:      https://github.com/JonZeolla/Development
-# Description:     This is a python script that accepts the output of `aws sts get-session-token` as stdin to create the appropriate environment variables make it transiently effective.
+# Description:     This is a python script that accepts the output of `aws sts get-session-token` as stdin to create the appropriate environment variables (making it transiently effective).
 #
 # Notes
-# - This plays much more nicely in a zsh environment than it does in bash.
+# - This plays much more nicely in a zsh environment than it does in bash based off of how it handles pipelining.
 #
 # =========================
 
@@ -24,4 +24,3 @@ except:
 print('export AWS_ACCESS_KEY_ID="%s"' % data['Credentials']['AccessKeyId'])
 print('export AWS_SECRET_ACCESS_KEY="%s"' % data['Credentials']['SecretAccessKey'])
 print('export AWS_SESSION_TOKEN="%s"' % data['Credentials']['SessionToken'])
-
